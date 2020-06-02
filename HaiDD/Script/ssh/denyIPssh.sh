825c108d59bb0ac3d0caca21c00eecb64e96349f
Đặng Đỗ Hải <danghai.hy.1996@gmail.com>
2020-06-02 16:03:22 +0700

    Create denyIPssh.sh

---
+++ HaiDD/Script/ssh/denyIPssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum -y install tcp_wrappers
+
+sudo apt-get install tcp_wrappers
+
+echo "ALL: ALL" >> /etc/hosts.deny
+
+echo "sshd: 10.10.35.197" >> /etc/hosts.allow
+
+echo "sshd: 10.10.34.197" >> /etc/hosts.allow
