0f61fac5b7b8fba728a47234992f3f89ce30ea5e
Đặng Đỗ Hải <danghai.hy.1996@gmail.com>
2020-12-03 17:34:18 +0700

    Update check_ssl.sh

--- HaiDD/Script/check-ssl/check_ssl.sh
+++ HaiDD/Script/check-ssl/check_ssl.sh
@@ -2,11 +2,16 @@
 
 # Check thoi han SSL cac domain
 input="/root/check_ssl/list_domain"
-now=``
+timestamp_now=`date +%s`
 while IFS= read -r line
 do  
     echo "$line"
     expired=`(echo | openssl s_client -servername $line -connect $line:443 2> /dev/null | openssl x509 -noout -enddate | cut -d "=" -f2)`
+    expired=`date -d"$expired" +'%d-%m-%Y %T'`
+    
+    # Tinh so ngay con lai cua chung chi SSL
+    
+
     echo "$expired"
     echo  -e "\n"
 done < "$input"
@@ -16,3 +21,5 @@ done < "$input"
 
 expired=`(echo | openssl s_client -servername cloud365.vn -connect cloud365.vn:443 2> /dev/null | openssl x509 -noout -enddate)`
 echo "$expired"
+
+date -d $expired +'%d-%m-%Y
