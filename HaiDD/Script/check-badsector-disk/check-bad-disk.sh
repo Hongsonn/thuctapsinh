896acc37ad16fd0549e4f3c235559b69b4bb6b45
Đặng Đỗ Hải <danghai.hy.1996@gmail.com>
2020-12-18 16:51:12 +0700

    Create check-bad-disk.sh

---
+++ HaiDD/Script/check-badsector-disk/check-bad-disk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ID chat Telegram
+USERID="-498240142"
+
+# API Token bot
+TOKEN="1290803372:AAGcoKflF3IL83-UFj_HvBiEMWr7vRJ9hAQ"
+
+# List disk
+INPUT="/root/nhanhoa-scripts/scripts/check_bad_disk/list_disk"
+
+# HOSTNAME
+HOSTNAME=`hostname`
+
+# Message report
+TEXT=`echo -e "$HOSTNAME"`
+
+while IFS= read -r line
+do
+    echo "$line"
+    RE_SEC_VALUE=`smartctl -a /dev/$line | grep 'Reallocated_Sector_Ct' | awk '{print $10}'`
+    if [$RE_SEC_VALUE > 0] > /dev/null 2>&1; then {
+        POH=`smartctl -a /dev/$line | grep 'Power_On_Hours' | awk '{print $10}'`
+        # Nội dung report
+        TEXT=$(echo -e "$TEXT\n$line\tReallocated_Sector_Ct: $RE_SEC_VALUE\tPower_On_Hours: $POH")
+    } else {
+        POH=0
+    } fi
+    echo "$RE_SEC_VALUE"
+done < "$INPUT"
