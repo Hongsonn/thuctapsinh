527943da6fce88871c332f16f3552cffd06f7624
haidd <haidd@nhanhoa.com.vn>
2020-12-22 17:38:59 +0700

    Update check-bad-disk.sh

--- HaiDD/Script/check-badsector-disk/check-bad-disk.sh
+++ HaiDD/Script/check-badsector-disk/check-bad-disk.sh
@@ -17,23 +17,22 @@ INPUT="/root/nhanhoa-scripts/scripts/check_bad_disk/list_disk"
 # HOSTNAME
 HOSTNAME=`hostname`
 
+# TIMEDATE
+DATE=`date +%d-%m-%Y`
+
 # Message report
-TEXT=`echo -e "$HOSTNAME\n-------"`
+TEXT=`echo -e "[ $HOSTNAME | $DATE ]\n-------"`
 
 while IFS= read -r line
 do
-    echo "$line"
     # Reallocated_Sector_Ct
     RSC=`smartctl -a /dev/$line | grep 'Reallocated_' | awk '{print $10}'`
-    echo "$RSC"
 
     # Uncorrectable_Error_Cnt
     UEC=`smartctl -a /dev/$line | grep "Uncorrectable_" | awk '{print $10}'`
-    echo "$UEC"
 
     # Pending_Sector_Count
     PSC=`smartctl -a /dev/$line | grep "Pending_" | awk '{print $10}'`
-    echo "$PSC"
     
     # Power_On_Hours
     POH=`smartctl -a /dev/$line | grep 'Power_On_Hours' | awk '{print $10}'`
@@ -41,12 +40,12 @@ do
     if [ "$RSC" != '' ] > /dev/null 2>&1; then {
         if [ "$RSC" != '0' ] || [ "$UEC" != '0' ] || [ "$PSC" != '0' ] > /dev/null 2>&1; then {
             # Nội dung report
-            TEXT=$(echo -e "$TEXT\n$line\nReallocated_Sector_Ct: $RSC\nPower_On_Hours: $POH\nUncorrectable_Error_Cnt: $UEC\nPending_Sector_Count: $PSC\n-------\n")
+            TEXT=$(echo -e "$TEXT\n[$line] - [WARN]\n- Reallocated_Sector_Ct: $RSC\n- Power_On_Hours: $POH\n- Uncorrectable_Error_Cnt: $UEC\n- Pending_Sector_Count: $PSC\n-------\n")
         } else {
-            TEXT=$(echo -e "$TEXT\n$line\n-------\n")
+            TEXT=$(echo -e "$TEXT\n[$line]\n-------\n")
         } fi
     } else {
-        TEXT=$(echo -e "$TEXT\n$line\nỔ cài OS\n-------\n")
+        TEXT=$(echo -e "$TEXT\n[$line]\n- Ổ cài OS\n-------\n")
     } fi
 done < "$INPUT"
 
